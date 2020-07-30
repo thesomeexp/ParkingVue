4d59ffd537166d390e4919531ba5fa5761dd952a
Shawn <1063040257@qq.com>
2020-07-30 16:07:09 +0800

    Global

---
+++ src/utils/Global.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "Global",
+    pre_url: "http://192.168.52.67/",
+    gateway: "http://localhost:9001/"
+  }
+</script>
+
+<style scoped>
+
+</style>
